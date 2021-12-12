d71ca8bdcc2273719cfb0910d4dc392e9ade39dd
h80r <h80rgbc@gmail.com>
2021-12-12 13:54:24 -0300

    Add Neuron documentation

--- neural_network/neuron.dart
+++ neural_network/neuron.dart
@@ -2,6 +2,8 @@ import 'dart:math';
 
 import 'math.dart';
 
+/// Neuron creates a neuron with [weightsCount] weights, randomly generated
+/// between -1 and 1.
 class Neuron {
   Neuron({
     required int weightsCount,
@@ -12,12 +14,19 @@ class Neuron {
           (index) => _random.nextDouble() * 2 - 1,
         );
 
+  /// The pseudo-random number generator used to generate weights.
   static final _random = Random();
 
+  /// The learning rate of the neuron.
   final double learningRate;
+
+  /// The inputs to the neuron.
   List<double> inputs;
+
+  /// The weights of the neuron.
   final List<double> weights;
 
+  /// Returns the output of the neuron.
   double evaluate() {
     var index = 0;
     final sum = inputs.fold<double>(
@@ -28,6 +37,7 @@ class Neuron {
     return tanh(sum);
   }
 
+  /// Updates the weights of the neuron.
   void train(double error) {
     final delta = (1 - pow(evaluate(), 2)) * error;
 
